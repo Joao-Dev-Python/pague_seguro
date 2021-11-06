96ce5a2d2ab02ff394e9bf803615cacbf9b0746a
Joao-Dev-Python <joaoxxt.ja@gmail.com>
2021-11-06 15:46:28 -0300

    hooh

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def web_hooks():
      result = request.get_json()
      data.append(*result)
      p  =pix.PixModel()
-     p.check_cob(txid= result[0].get("txid") )
+     p.check_cob(txid= dict(*result).get("txId") )
      return jsonify({
             "message" : "sucess" }),200
 
 if __name__== '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True,host="0.0.0.0",port=port)
+    app.run(debug=True,port=port)
     #,host="0.0.0.0"
