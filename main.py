c6468bfeb863ef696d56384b43912411a9fd8205
Joao-Dev-Python <44150035+Joao-Dev-Python@users.noreply.github.com>
2022-05-05 12:45:29 -0300

    hooks-up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+'''from flask import Flask,request,jsonify
 from flask_cors import CORS
 import os,pix
 
@@ -29,3 +29,29 @@ if __name__== '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,host="0.0.0.0",port=port)
     #,host="0.0.0.0"
+    '''
+
+
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
+events = []
+
+
+@app.route("/webhooks",methods= ['POST'])
+def post_events():
+    data = request.get_json()
+    events.append(data)
+    return jsonify(data)
+
+@app.route("/",methods=['GET'])
+def get_events():
+    return jsonify(events)
+
+
+if __name__== '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host="0.0.0.0",port=port)
+#,host="0.0.0.0"
+
